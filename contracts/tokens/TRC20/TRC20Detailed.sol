f18e72cf050b6cb676a674ded90221ff1c5908ba
Eric Chen <eric@tron.network>
2018-12-13 17:15:12 -0800

    Add TRC20Detailed to support name, symbol and decimals.

---
+++ contracts/tokens/TRC20/TRC20Detailed.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.23;
+
+import "./TRC20.sol";
+
+/**
+ * @title TRC20Detailed token
+ * @dev The decimals are only for visualization purposes.
+ * All the operations are done using the smallest and indivisible token unit,
+ * just as on TRON all the operations are done in sun.
+ *
+ * Example inherits from basic TRC20 implementation but can be modified to
+ * extend from other ITRC20-based tokens:
+ * https://github.com/OpenZeppelin/openzeppelin-solidity/issues/1536
+ */
+contract TRC20Detailed is TRC20 {
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor (string name, string symbol, uint8 decimals) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+    }
+
+    /**
+     * @return the name of the token.
+     */
+    function name() public view returns (string) {
+        return _name;
+    }
+
+    /**
+     * @return the symbol of the token.
+     */
+    function symbol() public view returns (string) {
+        return _symbol;
+    }
+
+    /**
+     * @return the number of decimals of the token.
+     */
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,11 @@
 var TRC20 = artifacts.require("./TRC20.sol");
+var TRC20Detailed = artifacts.require("./TRC20Detailed.sol");
 var TRC20Mintable = artifacts.require("./TRC20Mintable.sol");
 var TRC20Burnable = artifacts.require("./TRC20Burnable.sol");
 
-
 module.exports = function (deployer) {
     deployer.deploy(TRC20, 10000);
+    deployer.deploy(TRC20Detailed, "TOKEN", "SYM", 18);
     deployer.deploy(TRC20Mintable, 10000);
     deployer.deploy(TRC20Burnable, 10000);
 };
